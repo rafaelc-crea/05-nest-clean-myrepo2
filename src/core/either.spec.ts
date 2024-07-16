929e268d2cb8c60a423d471a8cc5353b7b798952
root <root@creago-mint-20.3>
2024-07-16 08:51:41 -0300

    first commit try

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from '@/core/either'
+
+function doSomeThing(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10)
+  } else {
+    return left('error')
+  }
+}
+
+test('success result', () => {
+  const result = doSomeThing(true)
+
+  if (result.isRight()) {
+    console.log(result.value)
+  }
+
+  expect(result.isRight()).toBe(true)
+  expect(result.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const result = doSomeThing(false)
+
+  expect(result.isLeft()).toBe(true)
+  expect(result.isRight()).toBe(false)
+})
---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,70 @@
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentsRepository } from '../repositories/answer-attachments-repository'
+
+interface EditAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+  attachmentsIds: string[]
+}
+
+type EditAnswerUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    answer: Answer
+  }
+>
+
+export class EditAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+    attachmentsIds,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      return left(new NotAllowedError())
+    }
+
+    const currentAnswerattachments =
+      await this.answerAttachmentsRepository.findManyByAnswerId(answerId)
+
+    const answerattachmentList = new AnswerAttachmentList(
+      currentAnswerattachments,
+    )
+
+    const answerattachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        answerId: answer.id,
+      })
+    })
+
+    answerattachmentList.update(answerattachments)
+
+    answer.attachments = answerattachmentList
+    answer.content = content
+
+    await this.answersRepository.save(answer)
+
+    return right({ answer })
+  }
+}
